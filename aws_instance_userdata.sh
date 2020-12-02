c4fac14874e39f88e7951e11cfadee4422b1c68a
DeepGamit <deepkgamit@gmail.com>
2020-12-02 03:01:24 -0500

    Assignment 9 Updates

--- aws_instance_userdata.sh
+++ aws_instance_userdata.sh
@@ -8,3 +8,6 @@ sudo echo 'DB_PASSWORD='${aws_db_password}'' >> /home/ubuntu/.env
 sudo echo 'DATABASE='${aws_db_name}'' >> /home/ubuntu/.env
 sudo echo 'AWS_REGION='${aws_region}'' >> /home/ubuntu/.env
 sudo echo 'AWS_BUCKET_NAME='${s3_bucket_name}'' >> /home/ubuntu/.env
+sudo echo 'DOMAIN_NAME='${aws_domainName}'' >> /home/ubuntu/.env
+sudo echo 'AWS_ENVIORMENT='${aws_environment}'' >> /home/ubuntu/.env
+sudo echo 'AWS_TOPIC_ARN='${aws_topic_arn}'' >> /home/ubuntu/.env
